b04f56c5dc65fc31fff17f117984dae8379fecd6
Risabh Rai <risabhrai2001@gmail.com>
2022-07-26 18:38:42 +0530

    smoothening zoom out

--- app.js
+++ app.js
@@ -34,22 +34,40 @@ canvas.on('mouse:wheel', function (opt) {
     var zoom = canvas.getZoom();
     zoom *= 0.999 ** delta;
     if (zoom > 20) { // Max zoom in to 20x
-        zoom = 20
+        zoom = 20;
     };
-    if (zoom < 1) { // Max zoom out to original size (1x)
-        zoom = 1;
+    if (zoom < 0.1) { // Max zoom out to original size (1x)
+        zoom = 0.1;
     }
 
     // accesing coordinates of viewport to calculate center
     const vptCoords = canvas.vptCoords;
+    const vpt = canvas.viewportTransform;
     if (delta > 0) { // In case of zooming out shift to center
         const centerX = (vptCoords.bl.x + vptCoords.br.x) / 2;
-        const centerY = (vptCoords.bl.y + vptCoords.br.y) / 2;
-        if (zoom < 1.5) {
+        const centerY = (vptCoords.bl.y + vptCoords.tl.y) / 2;
+        if (zoom < 0.15) {
             // centers image when image zooms out close to original size
             img.viewportCenter();
         }
         canvas.zoomToPoint({ x: centerX, y: centerY }, zoom);
+        console.log({ centerX, centerY }, opt);
+        vpt[4] = centerX;
+        vpt[5] = centerY;
+
+        // if (opt.e.offsetX < 300 && opt.e.offsetY < 300) {
+        //     canvas.zoomToPoint({ x: 300 - opt.e.offsetX, y: 300 - opt.e.offsetY }, zoom);
+        // }
+        // else if (opt.e.offsetX > 300 && opt.e.offsetY > 300) {
+        //     canvas.zoomToPoint({ x: opt.e.offsetX - 300, y: opt.e.offsetY - 300 }, zoom);
+        // }
+        // else if (opt.e.offsetX > 300 && opt.e.offsetY < 300) {
+        //     canvas.zoomToPoint({ x: opt.e.offsetX - 300, y: 300 - opt.e.offsetY }, zoom);
+        // }
+        // else if (opt.e.offsetX < 300 && opt.e.offsetY > 300) {
+        //     canvas.zoomToPoint({ x: 300 - opt.e.offsetX, y: opt.e.offsetY - 300 }, zoom);
+        // }
+
     }
     else { // zoom to point where mouse points
         canvas.zoomToPoint({ x: opt.e.offsetX, y: opt.e.offsetY }, zoom);
